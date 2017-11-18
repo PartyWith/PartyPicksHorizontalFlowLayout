03baf70462dc98ff3fd234db8fbf8472a85abcb1
Felipe Ricieri <felipericieri@icloud.com>
2017-11-18 11:53:53 -0200

    Example Project & Pod Base

---
+++ Pod/PartyPicksHorizontalFlowLayout.swift
@@ -0,0 +1,9 @@
+//
+//  PartyPicksHorizontalFlowLayout.swift
+//  PartyPicksHorizontalFlowLayout
+//
+//  Created by Felipe Ricieri on 18/11/2017.
+//  Copyright © 2017 Ricieri ME. All rights reserved.
+//
+
+import Foundation
